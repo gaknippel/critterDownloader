6b2def7837daa01b8cd8c0720e8ffb22257c7daa
Greyson Knippel <gaknippel@hotmail.com>
2026-01-20 20:59:36 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "./components/app-sidebar"
+import { ThemeProvider } from "@/components/theme-provider"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+    </ThemeProvider>
+  )
+}
